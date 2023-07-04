57d9d2ff82f88f74031f44256f7843fcfdfbb932
coshcage <58808125+coshcage@users.noreply.github.com>
2023-07-04 15:29:27 +0800

    Add files via upload

---
+++ src/silock.c
@@ -0,0 +1,31 @@
+/*
+ * Name:        silock.c
+ * Description: Lock functions.
+ * Author:      cosh.cage#hotmail.com
+ * File ID:     0704231516F0000000000L00030
+ * License:     GPLv2.
+ */
+
+#include "svimrdb.h"
+
+#define Y TRUE
+#define N FALSE
+
+typedef enum en_CmpMatHdr
+{
+	CMH_S = 0,
+	CMH_X,
+	CMH_IS,
+	CMH_IX,
+	CMH_SIX
+} CmpMatHdr;
+
+BOOL bCompatibleMatrix[][6] =
+{
+	{Y,N,Y,N,N,Y},
+	{N,N,N,N,N,Y},
+	{Y,N,Y,Y,Y,Y},
+	{N,N,Y,Y,N,Y},
+	{N,N,Y,N,N,Y},
+	{Y,Y,Y,Y,Y,Y}
+};
