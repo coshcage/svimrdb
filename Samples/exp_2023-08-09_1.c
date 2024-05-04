ee372fd8fbdcc473ed2eaed340e340ee8d75950f
John Cage <58808125+coshcage@users.noreply.github.com>
2024-05-04 11:54:37 +0000

    Add files via upload

--- Samples/exp_2023-08-09_1.c
+++ Samples/exp_2023-08-09_1.c
@@ -128,16 +128,10 @@ P_TABLE CreateTableSC(P_TRANS ptrans)
 
 void InsertIntoTableSC(P_TRANS ptrans, P_TABLE ptbl)
 {
-	float f;
-	f = 92.0f;
 	siInsertIntoTable(ptrans, ptbl, NULL, 2023001, 1, 92.0);
-	f = 85.0f;
 	siInsertIntoTable(ptrans, ptbl, NULL, 2023001, 2, 85.0);
-	f = 88.0f;
 	siInsertIntoTable(ptrans, ptbl, NULL, 2023001, 3, 88.0);
-	f = 90.0f;
 	siInsertIntoTable(ptrans, ptbl, NULL, 2023002, 2, 90.0);
-	f = 80.0f;
 	siInsertIntoTable(ptrans, ptbl, NULL, 2023002, 3, 80.0);
 }
 
